14869ca782d07e0db32235d5c6fbacd81b1c737b
anissaabenn <107192853+anissaabenn@users.noreply.github.com>
2023-01-11 11:39:42 +0100

    Création fichier de l'accueil + appel au fichier sur l'index grâce à un require once

--- index.php
+++ index.php
@@ -1,45 +1,5 @@
-<?php require_once "view/header.view.html"; ?>
+<?php require_once "view/header.view.html"; 
 
-<main>
-    <div class="img_accueil">
-        <span class="badge">New</span>
-        <div class="newOffer">
-            <h1 class="title1">serums</h1>
-            <button class="btn" id="goOffer">SHOP NOW !</button>
-        </div>
-    </div>
-
-    <h2 class="title2">Les produits favoris</h2>
-
-
-    <div class="favproducts">
-        <div class="article">
-            <img class="photos" src="/utils/beauty/face/1.1.png">
-            <h3 class="h3">Nom de l'article</h3>
-            <h4 class="h4">Prix de l'article €</h4>
-            <button type="button" class="btn" id="add">Ajouter au panier</button>
-        </div>
-    </div>
-
-
-
-    <div class="valeurs">
-        <h5>Nos valeurs</h5>
-        <div class="all">
-            <div class="first">
-                <h6>Vegan</h6>
-                <p class="text1">Nous sommes vegans, ce qui signifie qu'aucun de nos produits, emballages, formules ou ingrédients ne contient de dérivés ou de sous-produits animaux</p>
-            </div>
-            <div class="second">
-                <h6>Clean</h6>
-                <p class="text2">Nos formules sont conçues avec des ingrédients pour assurer le juste équilibre entre efficacité, sécurité et responsabilité, produisant des formulations plus restrictives que les réglementations aux Etats-Unis, au Canada, dans l'UE et au-delà.</p>
-            </div>
-            <div class="third">
-                <h6>Paraben free</h6>
-                <p class="text3">Nous formulons sans parabènes, un conservateur controversé largement utilisé dans l'industrie cosmétique, car votre sécurité et votre santé sont notre priorité.</p>
-            </div>
-        </div>
-    </div>
-</main>
+require_once "view/home.view.php";?>
 
 <?php require_once "view/footer.view.html";
